cd8bafdc3518378304721b3fdb9aeb476da874a8
nekover2 <mpid291004@gmail.com>
2024-05-19 14:38:22 +0900

    clean

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
-from Models.ModelTracker import ModelTracker
-from Models.CornerDetector import CornerDetector
-from Utils.VideoProcessor import read_video, save_video
-from Utils.PlayerFilter import filter_players, filter_players_by_corner
-import os
-import cv2
+from Utils import VideoProcessor, ModelManager
 
 def main():
-    #read image 
-    image = cv2.imread('image.png')
-    # Read video
+    #read video
     input_video_path = 'video.mp4'
-    video_frames = read_video(input_video_path)
+    video_frames = VideoProcessor.read_video(input_video_path)
     
-    player_tracker = ModelTracker(model_path='yolov8s.pt')
-    court_tracker = ModelTracker(model_path='PretrainedModel/court.pt')
-    shuttlecock_tracker = ModelTracker(model_path='PretrainedModel/best.pt')
-    # for frame in video_frames:
-    court_coordinates = court_tracker.predict_frame(image)[0]
-    people_coordinates = player_tracker.track_frame([image])
-    
-    # Cut the court from the image
-    x1, y1, x2, y2 = court_coordinates
-    court_image = image[int(y1):int(y2), int(x1):int(x2)]
+    model_manager = ModelManager.ModelManager(player_model_path='yolov8s.pt', court_model_path='PretrainedModel/courtModel.pt')
+    model_manager.load_models()
 
-    # Detect the corners of the court
-    corner_detector = CornerDetector(court_image)
-    corner_coordinates = corner_detector.convert_coordiante_size([court_coordinates])
-    # We will take the first 4 corners as the court corners
-    #cv2.imwrite('court.png', court_image)
-    image = court_tracker.draw_boxes(image, [court_coordinates])
+    model_manager.start(video_frames)
+    frames = model_manager.draw_boxes(video_frames)
+    player_spds= model_manager.calculate_player_speeds(video_frames)
+    print(len(player_spds))
+    print(len(frames))
+    frames = model_manager.show_calculate_player_speeds(frames)
+    VideoProcessor.save_video(frames, 'output.mp4')
     
     
-    frames = corner_detector.draw_key_points_on_videos([image], [court_coordinates])
-    
-    cv2.imwrite('output1.png', frames[0])
-    
-    true_player_coordinates = filter_players_by_corner(people_coordinates, corner_coordinates)
-    # filter out the player coordinates that are within the court, then draw the bounding boxes
-    print(corner_coordinates)
-    print(true_player_coordinates)
-            
-    image = player_tracker.draw_boxes(image, true_player_coordinates)
-    #save image
-    cv2.imwrite('output.png', image)
-
-
 if __name__ == "__main__":
     main()
